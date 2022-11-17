a13ff3ce49de090b22bb35af286bd2dd566360ea
Dustin Zook <dustin.w.zook@gmail.com>
2022-11-17 14:50:48 -0600

    finished circles exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,14 +1,23 @@
 package shapes;
+import util.Input;
 
 public class Circle {
     private double radius;
-    public Circle(double radius){
-        this.radius = radius;
+    public Circle(){
+        radius = Input.getDouble();
+        System.out.println("radius: "+this.radius);
+        System.out.println("Area: "+getArea());
+        System.out.println("Circumference: "+getCircumference());
     }
-    public double getArea(double radius){
-        return Math.PI* (radius*radius);
+    public double getArea(){
+        return (Math.PI * (this.radius*this.radius));
     }
     public double getCircumference(){
-        return 2*Math.PI*radius;
+        return (2 * Math.PI * this.radius);
+    }
+
+    public static void main(String[] args){
+        System.out.println("enter the radius of the circle");
+        Circle userCircle = new Circle();
     }
 }
